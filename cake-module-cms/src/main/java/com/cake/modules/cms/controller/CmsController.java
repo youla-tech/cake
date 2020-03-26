bfe2b91ba223d0a734052c843182f20373532b00
eamiear <justcall_me_k@163.com>
2020-03-26 17:57:49 +0800

    cake-module-cms

---
+++ cake-module-cms/src/main/java/com/cake/modules/cms/controller/CmsController.java
@@ -0,0 +1,13 @@
+package com.cake.modules.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Kezai_Su on 2020/3/26.
+ */
+@Controller
+@RequestMapping("/cms")
+public class CmsController {
+
+}
---
+++ cake-module-cms/src/main/java/com/cake/modules/cms/service/ArticleService.java
@@ -0,0 +1,7 @@
+package com.cake.modules.cms.service;
+
+/**
+ * Created by Kezai_Su on 2020/3/26.
+ */
+public class ArticleService {
+}
